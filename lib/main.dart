0b72654eb83098dd1d4a4374eae0f72ff25eee7a
Omar Sameh <omar.sameh.m.i@gmail.com>
2026-04-03 21:20:05 +0200

    Update lib/main.dart with new implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,17 @@
 import 'package:amazon_clone/blocs/user_details/user_details_bloc.dart';
+import 'package:amazon_clone/config/firebase_config.dart';
 import 'package:amazon_clone/layout/screen_layout.dart';
 import 'package:amazon_clone/utils/colors_theme.dart';
 import 'package:amazon_clone/views/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyCMBax2i83OUg8uSQYDQkhzBR3jO2ybOM8",
-        authDomain: "clone-2c230.firebaseapp.com",
-        projectId: "clone-2c230",
-        storageBucket: "clone-2c230.firebasestorage.app",
-        messagingSenderId: "101011406058",
-        appId: "1:101011406058:web:b4854fdd0ec8be49b0eb93",
-      ),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
+  // Initialize Firebase securely using configuration handler
+  await FirebaseConfig.initializeFirebase();
 
   runApp(const AmazonClone());
 }
